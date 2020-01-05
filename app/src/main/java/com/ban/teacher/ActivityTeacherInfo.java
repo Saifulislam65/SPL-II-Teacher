48e4dc396074e068bff32f2f109339e2e2c60e9b
Saiful Islam Niloy <52848678+Saifulislam65@users.noreply.github.com>
2020-01-05 12:18:39 +0600

    commit

--- app/src/main/java/com/ban/teacher/ActivityTeacherInfo.java
+++ app/src/main/java/com/ban/teacher/ActivityTeacherInfo.java
@@ -3,11 +3,11 @@ package com.ban.teacher;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActivityStudentInfo extends AppCompatActivity {
+public class ActivityTeacherInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_info);
+        setContentView(R.layout.activity_teacher_info);
     }
 }
